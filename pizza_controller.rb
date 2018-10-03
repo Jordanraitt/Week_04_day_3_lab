9cd0b1a9d4a6a1d5851a49ee90577e5f7b3528be
jordanraitt <jordanraitt@hotmail.com>
2018-10-03 14:59:15 +0100

    start

---
+++ pizza_controller.rb
@@ -0,0 +1,30 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( './models/pizza_order' )
+also_reload( './models/*' )
+
+# this is our index route
+get('/pizza-orders') do
+  @pizzas = PizzaOrder.all()
+  erb(:index)
+end
+
+#new route
+get('/pizza-orders/new')do
+  erb(:new)
+end
+
+# show route
+get('/pizza-orders/:id') do
+  id = params[:id].to_i()
+  @pizza = PizzaOrder.find(id)
+  erb(:show)
+end
+
+#create route
+post('/pizza-orders') do
+  @pizza = PizzaOrder.new(params)
+  @pizza.save()
+  erb(:create)
+end
